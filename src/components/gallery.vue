6f18b73e2c16de0f2f7c098670ae9de7902488fd
Nima Shahbazi <nimashahbazi524@gmail.com>
2020-11-16 03:04:11 +0330

    add cover option to pictures

--- src/components/gallery.vue
+++ src/components/gallery.vue
@@ -1,13 +1,19 @@
 <template>
     <div class="gallery-container">
         <div class="images-box">
-            <div class="img-1"></div>
-            <div class="img-2"></div>
-            <div class="img-3"></div>
-            <div class="img-4" 
+            <div class="img-1"
+                @click="img1Clicked">
+            </div>
+            <div class="img-2"
+                @click="img2Clicked">
+            </div>
+            <div class="img-3"
+                @click="img3Clicked">
+            </div>
+            <div class="img-4"
+                @click="img4Clicked" 
                 @mouseenter="img4MouseEnter" 
-                @mouseleave="img4MouseLeave"
-                ref="img4">
+                @mouseleave="img4MouseLeave">
             </div>
         </div>
     </div>
@@ -15,15 +21,35 @@
 
 <script>
 export default {
+    data(){
+        return{
+            img1: document.getElementsByClassName('img-1'),
+            img2: document.getElementsByClassName('img-2'),
+            img3: document.getElementsByClassName('img-3'),
+            img4: document.getElementsByClassName('img-4')
+        }
+    },
     methods: {
         img4MouseEnter(){
-            console.log(document.getElementsByClassName('img-4')[0].style);
-            document.getElementsByClassName('img-4')[0].style.zIndex = 11;
+            this.img4[0].style.zIndex = 11;
         },
         img4MouseLeave(){
             setTimeout(() => {
-                document.getElementsByClassName('img-4')[0].style.zIndex = 1;
-            } , 1200)
+                this.img4[0].style.zIndex = 1;
+            } , 1200);
+        },
+
+        img1Clicked(){
+            this.img1[0].classList.toggle('img1-cover');
+        },
+        img2Clicked(){
+            this.img2[0].classList.toggle('img2-cover');
+        },
+        img3Clicked(){
+            this.img3[0].classList.toggle('img3-cover');
+        },
+        img4Clicked(){
+            this.img4[0].classList.toggle('img4-cover');
         }
     }
 }
@@ -41,63 +67,91 @@ export default {
         clip-path: polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%);
         background: #101010;
         position: relative;
+        // image 1 styles
         .img-1{
             width: inherit;
             height: inherit;
-            clip-path: polygon(25% 0%, 75% 0%, 100% 50% , 100% 50%);
+            clip-path: polygon(25% 0%, 75% 0%, 100% 50%, 100% 50%, 100% 50%, 100% 50%);
             position: absolute;
             @include image('../assets/img-1.jpg');
-            transition: clip-path 1200ms ease;
+            transition: 1200ms ease;
             z-index: 10; 
             cursor: pointer;
 
             &:hover{
-                clip-path: polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%);
+                clip-path: polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 75% 100%, 75% 100%);
             }
         }
+        .img1-cover{
+            transition: all 1200ms;
+            clip-path: polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%) !important;
+            z-index: 100;
+        }
+        // image 2 styles
         .img-2{
             width: inherit;
             height: inherit;
-            clip-path: polygon(25% 0%, 100% 50%, 75% 100%, 75% 100%, 75% 100%);
+            clip-path: polygon(25% 0%, 100% 50%, 75% 100%, 75% 100%, 75% 100%, 75% 100%);
             position: absolute;
             @include image('../assets/img-5.jpg');
-            transition: clip-path 1200ms ease;
+            transition: 1200ms ease;
             z-index: 9;
             cursor: pointer;
 
             &:hover{
-                clip-path: polygon(25% 0%, 100% 50%, 100% 50%, 75% 100%, 25% 100%);
+                clip-path: polygon(25% 0%, 100% 50%, 100% 50%, 75% 100%, 25% 100%, 25% 100%);
             }
         }
+        .img2-cover{
+            transition: all 1200ms;
+            clip-path: polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%) !important;
+            z-index: 100;
+        }
+        // image 3 styles
         .img-3{
             width: inherit;
             height: inherit;
-            clip-path: polygon(25% 0%, 75% 100%, 25% 100%, 25% 100%);
+            clip-path: polygon(25% 0%, 75% 100%, 75% 100%, 75% 100%, 25% 100%, 25% 100%);
             position: absolute;
             @include image('../assets/img-6.jpg');
-            transition: clip-path 1200ms ease;
+            transition: 1200ms ease;
             z-index: 8;
             cursor: pointer;
 
             &:hover{
-                clip-path: polygon(25% 0%, 75% 100%, 25% 100%, 0% 50%);
+                clip-path: polygon(25% 0%, 75% 100%, 75% 100%, 75% 100%, 25% 100%, 0% 50%);
             }
         }
+        .img3-cover{
+            transition: all 1200ms;
+            clip-path: polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%) !important;
+            z-index: 100;
+        }
+        // image 4  styles
         .img-4{
             width: inherit;
             height: inherit;
-            clip-path: polygon(25% 0%, 25% 100%, 25% 100%, 0% 50%);
+            clip-path: polygon(25% 0%, 25% 100%, 25% 100%, 25% 100%, 25% 100%, 0% 50%);
             position: absolute;
             @include image('../assets/img-8.jpg');
-            transition: clip-path 1200ms ease;
+            transition: 1200ms ease;
             cursor: pointer;
 
             &:hover{
-                clip-path: polygon(25% 0%, 75% 100%, 25% 100%, 0% 50%);
+                clip-path: polygon(25% 0%, 75% 100%, 75% 100%, 75% 100%, 25% 100%, 0% 50%);
             }
+        }
+        .img4-cover{
+            transition: all 1200ms;
+            clip-path: polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%) !important;
+            z-index: 100;
         }
 
         @media only screen and (max-width: 650px){
+            width: 25rem;
+            height: 25rem;
+        }
+        @media only screen and (max-width: 450px){
             width: 20rem;
             height: 20rem;
         }
